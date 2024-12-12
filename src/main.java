f6173d2675c46efa7e33df7304f90ddb0b8c29ee
Lex <74861308+Kekistanstan@users.noreply.github.com>
2024-12-12 22:35:27 +0800

    Pinakaunang commit nasad

---
+++ src/main.java
@@ -0,0 +1,21 @@
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application{
+  @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Turing Machine Simulsator");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
